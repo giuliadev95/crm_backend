7004c5d89e41de274a59135719b13f119b9e56a7
giulia_moukouyou <giuliamoukouyou@gmail.com>
2025-05-16 12:02:08 +0200

    Fixed express import in server.ts.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 /* Configuration of the server */
-import express from 'express';
+
 import { Request, Response} from 'express';
 import mongoose from 'mongoose';
 // simport cors from 'cors';
 
 // Constants app, port, connection string
+const express = require('express')
 const app = express();
 const port = 5000;
 const db_ulr = "mongodb://localhost:27017/corsomongo"; // To put in .env
